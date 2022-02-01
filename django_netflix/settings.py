26721c563460ba55ef8f6c383d9ac60f73390460
Bluette1 <marylene.sawyer@gmail.com>
2022-02-01 06:17:52 +0200

    Styling - Login/Logout forms

--- django_netflix/settings.py
+++ django_netflix/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from corsheaders import django
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    # Third party apps
+     'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'django.contrib.sites'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +138,18 @@ STATICFILES_DIRS = [
 
 # Auth String
 AUTH_USER_MODEL = 'core.CustomUser'
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+ACCOUNT_AUTHENTICATION_METHOD='email'
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_EMAIL_VERIFICATION='none'
+ACCOUNT_USERNAME_REQUIRED=False
+LOGIN_REDIRECT_URL='/'
